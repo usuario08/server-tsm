8a6fcf8b235fe5ae029f6bd90e9cbbc6bbb0180b
mejia <aamc5991@gmail.com>
2023-12-29 21:58:05 -0500

    mejoras

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,22 @@
-import { ManagerEntity } from '../context/maestros/users/bootstrap'
+import { globSync } from 'glob'
+import path from 'path'
 
 export class InitDB {
 
   constructor() { }
 
   async exec() {
-    await new ManagerEntity().exec()
+    await this.initializeBootstraps()
   }
-  
+
+  private async initializeBootstraps() {
+    const paths = globSync('src/context/**/bootstrap.ts', { stat: true, withFileTypes: true })
+    for await (const rute of paths) {
+      const newPath = path.join('../', rute.fullpath().split('src')[1].replace('.ts', ''))
+      const { ManagerEntity } = await import(newPath)
+      const controller = new ManagerEntity()
+      await controller.exec()
+    }
+  }
+
 }
