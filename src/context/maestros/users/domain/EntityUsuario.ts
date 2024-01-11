770a3af9a9e4ecafde1ab6a2722b68d4511759a8
mejia <aamc5991@gmail.com>
2024-01-11 06:31:52 -0500

    changes

--- src/context/maestros/users/domain/EntityUsuario.ts
+++ src/context/maestros/users/domain/EntityUsuario.ts
@@ -7,7 +7,7 @@ export class EntityUsuario {
   apellidoPaterno: string = ''
   apellidoMaterno: string = ''
   email: string = ''
-  empresa: CodigoNombreDto = new CodigoNombreDto()
+  // empresa: CodigoNombreDto = new CodigoNombreDto()
   perfil: CodigoNombreDto = new CodigoNombreDto()
   estado: boolean = true
   emailVerificado: boolean = false
